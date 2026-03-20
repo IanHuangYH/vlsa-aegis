b35a1889f6e9ff6e53f0f16654748b22038c94be
seaclear <no>
2026-03-20 10:30:32 +0100

    add debig mode and script to collect dataset

---
+++ collect_own_dataset.sh
@@ -0,0 +1,8 @@
+
+python safelibero/scripts/collect_demonstration.py \
+                    --controller OSC_POSE \
+                    --camera agentview --robots Panda \
+                    --device keyboard \
+                    --num-demonstration 5 \
+                    --rot-sensitivity 1.5 \
+                    --bddl-file /home/seaclear/Desktop/ian/code/vlsa-aegis/safelibero/libero/libero/bddl_files/safelibero_goal/put_the_bowl_on_the_plate.bddl
